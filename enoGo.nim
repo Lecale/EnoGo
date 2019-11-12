88ab012a0883f379e9596daff53a9a8a930592df
Lecale <sivadnai@gmail.com>
2019-11-12 23:21:09 +0100

    again

--- enoGo.nim
+++ enoGo.nim
@@ -1,18 +1,29 @@
 import random, strutils, terminal
 
 let contents = readFile("NeuralNet.txt").splitLines() 
+var consultations = 0
 
 while true :
   eraseScreen()
-  echo "Consult Eno? : (y/n)"
+  if consultations == 0 :
+    echo "Do you wish to consult Eno? : (y/n)"
+  else :
+    echo "Do you wish to consult Eno again? : (y/n)"
   let answer = readLine(stdin)
   if answer == "y" :
     echo ""
-    echo contents[rand(contents.len - 1)]
+    echo " This is your advice: "
+    echo ""    
+    echo "  " , contents[rand(contents.len - 1)]
     echo ""
+    echo " When you are ready. Press any key to continue"
+    echo ""
+    let peter = readLine(stdin)
+    consultations = consultations + 1
   if answer == "n" :
     break
 
 eraseScreen()
 echo ""
 echo "Thank you. Brian Eno is empty."
+echo "You consulted " , consultations , " times"
